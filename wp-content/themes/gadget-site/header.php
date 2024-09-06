c422e2055339fd09bbbf62aba71cbc2f97f56fe1
ptejalaxioned <tejalp@axioned.com>
2024-09-06 17:16:24 +0530

    html structure of header commit

--- wp-content/themes/gadget-site/header.php
+++ wp-content/themes/gadget-site/header.php
@@ -15,6 +15,26 @@
   <div class="container">
     <!--header section start-->
     <header>
+      <div class="wrapper">
+        <?php wp_nav_menu(array("theme_location" => "primary-menu", "menu_class" => "nav-list"))
+        ?>
+        <div class="header-center">
+          <a href="#FIXME" class="button" target="_self">contact us</a>
+        </div>
+        <div class="header-right">
+          <ul class="icons-list">
+            <li>
+              <a href="#FIXME" title="Cart" target="_self" class="cart">cart</a>
+            </li>
+            <li>
+              <a href="#FIXME" title="Search" target="_self" class="search">search</a>
+            </li>
+            <li>
+              <a href="#FIXME" title="Earth" target="_self" class="earth">earth</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
     <!--header section end-->
     <!--main section start-->
