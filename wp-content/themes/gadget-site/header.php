30f358c716f5de2ac7486f667571dec630d855da
ptejalaxioned <tejalp@axioned.com>
2024-09-12 11:48:50 +0530

    header section file commit

--- wp-content/themes/gadget-site/header.php
+++ wp-content/themes/gadget-site/header.php
@@ -18,21 +18,52 @@
       <div class="wrapper">
         <?php wp_nav_menu(array("theme_location" => "primary-menu", "menu_class" => "nav-list"))
         ?>
-        <div class="header-center">
-          <a href="<?php echo get_field("header_button",'option') ?>" class="button" target="_self"><?php echo get_field("header_button_text",'option') ?></a>
-        </div>
+        <?php
+        $customize_link = get_field("customize_link", 'option');
+        $customize_link_title = get_field("customize_link", 'option')['title'];
+        ?>
+        <?php if ($customize_link) { ?>
+          <div class="header-center">
+            <a href="<?php echo $customize_link ?>" class="button" target="_self"><?php echo  $customize_link_title  ?></a>
+          </div>
+        <?php } ?>
+        <?php
+        $cart = get_field("cart", 'option');
+        $search = get_field("search", 'option');
+        $globe = get_field("globe", 'option');
+        $cart_link = $cart['icon_link'];
+        $cart_image = $cart['icon_image'];
+        $search_link =  $search['icon_link'];
+        $search_image =  $search['icon_image'];
+        $globe_link = $globe['icon_link'];
+        $globe_image =  $globe['icon_image'];
+        ?>
         <div class="header-right">
-          <ul class="icons-list">
-            <li>
-              <a href="#FIXME" title="Cart" target="_self" class="cart"><img src="<?php echo get_field("header_button",'option') ?>" alt=""></a>
-            </li>
-            <li>
-              <a href="#FIXME" title="Search" target="_self" class="search">search</a>
-            </li>
-            <li>
-              <a href="#FIXME" title="Earth" target="_self" class="earth">earth</a>
-            </li>
-          </ul>
+          <?php if ($cart || $search || $globe) { ?>
+            <ul class="icons-list">
+              <?php if ($cart) { ?>
+                <li>
+                  <a href="<?php echo $cart_link ?>" title="Cart" target="_self" class="cart">
+                    <img src="<?php echo  $cart_image ?>" alt="">
+                  </a>
+                </li>
+              <?php } ?>
+              <?php if ($search) { ?>
+                <li>
+                  <a href="<?php echo $search_link ?>" title="Cart" target="_self" class="">
+                    <img src=" <?php echo $search_image ?>" alt="">
+                  </a>
+                </li>
+              <?php } ?>
+              <?php if ($globe) { ?>
+                <li>
+                  <a href="<?php echo  $globe_link ?>" title="Globe" target="_self" class="globe">
+                    <img src=" <?php echo $globe_image ?>" alt="">
+                  </a>
+                </li>
+              <?php } ?>
+            </ul>
+          <?php } ?>
         </div>
       </div>
     </header>
