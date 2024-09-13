097bd39e7e6b00ac3f1ac660584f93d26a5a58bb
ptejalaxioned <tejalp@axioned.com>
2024-09-13 19:24:28 +0530

    function to add footer tab in theme setting  functions.php commit

--- wp-content/themes/gadget-site/functions.php
+++ wp-content/themes/gadget-site/functions.php
@@ -60,16 +60,16 @@ if (function_exists('acf_add_options_page')) {
     'menu_title'  => 'Header',            
     'parent_slug' => 'theme-general-settings', 
 ));
-acf_add_options_sub_page(array(
-  'page_title'  => 'Footer Settings',    
-  'menu_title'  => 'Footer',            
-  'parent_slug' => 'theme-general-settings', 
+  acf_add_options_sub_page(array(
+  'page_title'  => 'Footer Settings',
+  'menu_title'  => 'Footer',
+  'parent_slug' => 'theme-general-settings',
 ));
 }
 
 //enable classic editor and disable  gutenberg
-add_filter('use_block_editor_for_post', '__return_false', 10);
-add_filter('use_widgets_block_editor', '__return_false');
+// add_filter('use_block_editor_for_post', '__return_false', 10);
+// add_filter('use_widgets_block_editor', '__return_false');
 
 
 //category filter
