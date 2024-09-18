00fb6aebf7c33e2874c1adee46b5a1d424f0b436
ptejalaxioned <tejalp@axioned.com>
2024-09-18 19:18:15 +0530

    unwanted code remove from front-page.php commit

--- wp-content/themes/veggies-site/front.page.php
+++ wp-content/themes/veggies-site/front.page.php
@@ -1,29 +1,15 @@
 <?php
+
 /**
  * Template Name: HomePage
  */
 ?>
 <?php get_header(); ?>
  <?php
-    while (have_posts()) { 
-      the_post();
-      the_content();
-    }
-    ?>
-<?php
-$home_template_location = 'template-parts/home/content';
-// if (have_rows('home_page_section')){
-//   while (have_rows('home_page_section')) 
-//   {
-//   the_row();
-//     switch (get_row_layout()) {
-//       case 'veggies_banner':
-//         echo "lll";
-//         get_template_part($home_template_location, 'banner');
-//         break;
-//     }
-//   }
-// }
-?>
+  while (have_posts()) {
+    the_post();
+    the_content();
+  }
+  ?>
 <?php get_footer(); ?>
 
