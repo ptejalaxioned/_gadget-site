bf24680946959ec8633ab64843bc6099b626a79f
ptejalaxioned <tejalp@axioned.com>
2024-09-18 19:17:16 +0530

    About Us Block styling in about-us-block.css commit

--- wp-content/themes/veggies-site/front.page.php
+++ wp-content/themes/veggies-site/front.page.php
@@ -10,20 +10,5 @@
       the_content();
     }
     ?>
-<?php
-$home_template_location = 'template-parts/home/content';
-// if (have_rows('home_page_section')){
-//   while (have_rows('home_page_section')) 
-//   {
-//   the_row();
-//     switch (get_row_layout()) {
-//       case 'veggies_banner':
-//         echo "lll";
-//         get_template_part($home_template_location, 'banner');
-//         break;
-//     }
-//   }
-// }
-?>
 <?php get_footer(); ?>
 
