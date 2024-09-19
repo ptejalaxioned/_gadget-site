398d4591b7fca3655d4cc161f78541b1c9a9e751
ptejalaxioned <tejalp@axioned.com>
2024-09-19 12:14:02 +0530

    specials section conditions added in conteny-specials.php commit

--- wp-content/themes/veggies-site/inc/blocks/content-specials.php
+++ wp-content/themes/veggies-site/inc/blocks/content-specials.php
@@ -1,28 +1,32 @@
 <?php
 $heading = get_field('heading');
 $image_list = get_field('image_list');
-
 ?>
 
-<!--specials section start-->
-<section class="specials">
-  <div class="wrapper">
-    <div class="heading-span">
-      <h2 class="single-caps"><?php echo $heading ?></h2>
+<?php if ($heading || $image_list) { ?>
+  <!--specials section start-->
+  <section class="specials">
+    <div class="wrapper">
+      <?php if ($heading) { ?>
+        <div class="heading-span">
+          <h2 class="single-caps"><?php echo $heading ?></h2>
+          <span class="line-h"> line </span>
+        </div>
+      <?php } ?>
+      <?php if ($image_list) { ?>
+        <ul class="img-list">
+          <?php
+          foreach ($image_list as $image) {
+            $image_url = $image['url'];
+            $image_alt = $image['alt'];
+          ?>
+            <li><img src="<?php echo $image_url ?>" alt="<?php echo $image_alt ?>" /></li>
+          <?php
+          }
+          ?>
+        </ul>
+      <?php } ?>
     </div>
-    <?php if ($image_list) { ?>
-      <ul class="img-list">
-        <?php
-        foreach ($image_list as $image) {
-          $image_url= $image['url'];
-          $image_alt = $image['alt'];
-        ?>
-          <li><img src="<?php echo $image_url ?>" alt="<?php echo $image_alt ?>" /></li>
-        <?php
-        }
-        ?>
-      </ul>
-    <?php } ?>
-  </div>
-</section>
-<!--specials section end-->
+  </section>
+  <!--specials section end-->
+<?php } ?>
