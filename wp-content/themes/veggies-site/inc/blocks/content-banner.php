108904a19176e19581c5ce7e6ddde7cf40f7e9cb
ptejalaxioned <tejalp@axioned.com>
2024-09-19 12:09:58 +0530

    banner section conditions added in conteny-banner.php commit

--- wp-content/themes/veggies-site/inc/blocks/content-banner.php
+++ wp-content/themes/veggies-site/inc/blocks/content-banner.php
@@ -5,19 +5,26 @@ $background_image_url= $background_image['url'];
 $background_image_alt= $background_image['alt'];
 ?>
 
+<?php if($background_image || $banner_hedaing ){ ?>
 <!--banner section start-->
 <section class="banner">
           <div class="wrapper">
+          <?php if($background_image){ ?>
             <figure class="banner-image">
               <img src="<?php echo $background_image_url ?>" alt=" <?php echo $background_image_alt ?>" />
             </figure>
+            <?php }?>
             <div class="banner-content">
               <div class="heading-span">
+              <?php if($banner_hedaing){ ?>
                 <h1 class="single-caps">
                 <?php echo $banner_hedaing ?>
                 </h1>
+                <span class="line-h"> line </span>
+                <?php }?>
               </div>
             </div>
           </div>
         </section>
 <!--banner section end-->
+<?php }?>
