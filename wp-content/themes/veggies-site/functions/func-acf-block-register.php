9836b48345f974a23896529532d7089f6488fb55
ptejalaxioned <tejalp@axioned.com>
2024-09-20 15:22:00 +0530

    icons for block in func-acf-block-register.php commit

--- wp-content/themes/veggies-site/functions/func-acf-block-register.php
+++ wp-content/themes/veggies-site/functions/func-acf-block-register.php
@@ -1,5 +1,6 @@
 <?php
-function my_acf_init() {
+function my_acf_init()
+{
   register_acf_blocks('banner', 'banner', 'A custom banner block', '/inc/blocks/content-banner.php', 'admin-page');
   register_acf_blocks('turnip-green', 'turnip-green', 'A custom turnip-green block', '/inc/blocks/content-turnip-green.php', 'food');
   register_acf_blocks('specials', 'specials', 'A custom specials block', '/inc/blocks/content-specials.php', 'admin-comments');
@@ -7,9 +8,9 @@ function my_acf_init() {
   register_acf_blocks('menu', 'menu', 'A custom menu block', '/inc/blocks/content-menu.php', 'welcome-widgets-menus');
 }
 
-
-function register_acf_blocks($slug, $title, $description, $template_loc, $icon) {
-  if( function_exists('acf_register_block') ) {
+function register_acf_blocks($slug, $title, $description, $template_loc, $icon)
+{
+  if (function_exists('acf_register_block')) {
     acf_register_block(array(
       'name'              => $slug,
       'title'             => __($title),
@@ -18,17 +19,17 @@ function register_acf_blocks($slug, $title, $description, $template_loc, $icon) 
       'render_template' => get_theme_file_path() . $template_loc,
       'category'          => 'formatting',
       'icon'              => $icon,
-      'keywords'          => array( $slug ),
+      'keywords'          => array($slug),
     ));
   }
 }
-function my_acf_block_render_callback( $block ) {
+function my_acf_block_render_callback($block)
+{
   // convert name ("acf/testimonial") into path friendly slug ("testimonial")
   $slug = str_replace('acf/', '', $block['name']);
   // include a template part from within the "template-parts/block" folder
-  if( file_exists( get_theme_file_path("/inc/blocks/content-{$slug}.php") ) ) {
-      include( get_theme_file_path("/inc/blocks/content-{$slug}.php") );
+  if (file_exists(get_theme_file_path("/inc/blocks/content-{$slug}.php"))) {
+    include(get_theme_file_path("/inc/blocks/content-{$slug}.php"));
   }
 }
 add_action('acf/init', 'my_acf_init');
-?>
