e826d520846983b1893bab227fe0bf7920b2a897
ptejalaxioned <tejalp@axioned.com>
2024-09-20 12:57:22 +0530

    all attributes of a tags make dynamic commit

--- wp-content/themes/veggies-site/footer.php
+++ wp-content/themes/veggies-site/footer.php
@@ -4,7 +4,13 @@ $veggies_heading = get_field("veggies_heading", 'option');
 $address = get_field("address", 'option');
 $reservations_heading = get_field("reservations_heading", 'option');
 $email = get_field("email", 'option');
+$email_url = str_replace('http://', '', $email['url']);
+$email_title = $email['title'];
+$email_target = $email['target'];
 $phone_number = get_field("phone_number", 'option');
+$phone_number_url = str_replace('http://', '', $phone_number['url']);;
+$phone_number_title = $phone_number['title'];
+$phone_number_target = $phone_number['target'];
 $contact_form_heading = get_field("contact_form_heading", 'option');
 $name_placeholder = get_field("name_placeholder", 'option');
 $email_placeholder = get_field("email_placeholder", 'option');
@@ -12,18 +18,20 @@ $textarea_placeholder = get_field("textarea_placeholder", 'option');
 $button_text = get_field("button_text", 'option');
 $footer_left_text = get_field("footer_left_text", 'option');
 $bootstrap_theme_link = get_field("bootstrap_theme_link", 'option');
+$bootstrap_theme_link_target = $bootstrap_theme_link['target'];
 $bootstrap_theme_link_title = $bootstrap_theme_link['title'];
 $copyright_text = get_field("copyright_text", 'option');
 $facebook_icon = get_field("facebook_icon", 'option');
 $facebook_icon_image = $facebook_icon['image'];
 $facebook_icon_link = $facebook_icon['link'];
+$facebook_icon_link_target = $facebook_icon_link['target'];
 $facebook_icon_link_title = $facebook_icon_link['title'];
 $linkdin_icon = get_field("linkdin_icon", 'option');
 $linkdin_icon_image = $linkdin_icon['image'];
 $linkdin_icon_link = $linkdin_icon['link'];
+$linkdin_icon_link_target = $linkdin_icon_link['target'];
 $linkdin_icon_link_title = $linkdin_icon_link['title'];
 $backgroud_image = get_field("backgroud_image", 'option');
-
 ?>
 </main>
 <!--main section end-->
@@ -31,7 +39,7 @@ $backgroud_image = get_field("backgroud_image", 'option');
   $contact_heading || $veggies_heading || $address || $reservations_heading ||
   $email || $phone_number || $contact_form_heading || $name_placeholder || $email_placeholder ||
   $textarea_placeholder || $button_text || $bootstrap_theme_link || $copyright_text ||
-  $facebook_icon_image_url || $facebook_icon_link || $linkdin_icon_image_url || $linkdin_icon_link
+  $facebook_icon_image || $facebook_icon_link || $linkdin_icon_image || $linkdin_icon_link
   || $backgroud_image
 ) { ?>
   <!--footer section start-->
@@ -41,7 +49,7 @@ $backgroud_image = get_field("backgroud_image", 'option');
       if ($backgroud_image) { ?>
         <figure class="veggies">
           <?php
-          echo wp_get_attachment_image($backgroud_image, [1279,852], false);
+          echo wp_get_attachment_image($backgroud_image, [1279, 852], false);
           ?>
         </figure>
       <?php } ?>
@@ -81,19 +89,19 @@ $backgroud_image = get_field("backgroud_image", 'option');
                     <h3 class="all-caps"><?php echo $reservations_heading ?></h3>
                     <?php if ($email) { ?>
                       <a
-                        href="mailto:reservations@veggies.com"
-                        target="_blank"
+                        href="mailto:<?php echo $email_url ?>"
+                        target="<?php echo $email_target ?>"
                         class="email"
-                        title="Mail"
-                        rel="noopener noreferrer"><?php echo $email ?></a>
+                        title="<?php echo $email_title ?>"
+                        rel="noopener noreferrer"><?php echo $email_url ?></a>
                     <?php } ?>
                     <?php if ($phone_number) { ?>
                       <a
-                        href="tel:+48-202-555-0114"
-                        target="_blank"
+                        href="tel:<?php echo $phone_number_url ?>"
+                        target="<?php echo $phone_number_target ?>"
                         class="phone-number"
-                        title="Phone Number"
-                        rel="noopener noreferrer"><?php echo $phone_number ?></a>
+                        title="<?php echo $phone_number_title ?>"
+                        rel="noopener noreferrer"><?php echo $phone_number_url ?></a>
                     <?php } ?>
                 </li>
               <?php } ?>
@@ -159,21 +167,21 @@ $backgroud_image = get_field("backgroud_image", 'option');
             <a
               href="<?php echo $bootstrap_theme_link ?>"
               class="bootstrap first-caps"
-              title="Bootstrap Themes"
-              target="_self"><?php echo $bootstrap_theme_link_title ?></a>
+              title="<?php echo $bootstrap_theme_link_title ?>"
+              target="<?php echo $bootstrap_theme_link_target ?>"><?php echo $bootstrap_theme_link_title ?></a>
           <?php } ?>
           <?php if ($copyright_text) { ?>
             <?php echo $copyright_text ?>
           <?php } ?>
         </p>
       <?php } ?>
-      <?php if ($facebook_icon_image_url || $facebook_icon_link || $linkdin_icon_image_url || $linkdin_icon_link) { ?>
+      <?php if ($facebook_icon_image || $facebook_icon_link || $linkdin_icon_image || $linkdin_icon_link) { ?>
         <ul class="icon-list">
-          <?php if ($facebook_icon_image_url || $facebook_icon_link) { ?>
+          <?php if ($facebook_icon_image || $facebook_icon_link) { ?>
             <li class="icon">
               <a
                 href="<?php echo $facebook_icon_link ?>"
-                target="_blank"
+                target="<?php echo $facebook_icon_link_target ?>"
                 class="facebook social-media"
                 title="<?php echo $facebook_icon_link_title ?>"
                 rel="noopener noreferrer">
@@ -183,11 +191,11 @@ $backgroud_image = get_field("backgroud_image", 'option');
               </a>
             </li>
           <?php } ?>
-          <?php if ($linkdin_icon_image_url || $linkdin_icon_link) { ?>
+          <?php if ($linkdin_icon_image || $linkdin_icon_link) { ?>
             <li class="icon">
               <a
                 href="<?php echo $linkdin_icon_link ?>"
-                target="_blank"
+                target="<?php echo $linkdin_icon_link_target ?>"
                 class="linkdin social-media"
                 title="<?php echo $linkdin_icon_link_title ?>"
                 rel="noopener noreferrer">
---
+++ wp-content/themes/veggies-site/template-parts/home/content-banner.php
@@ -0,0 +1,7 @@
+<!--banner section start-->
+<section class="banner">
+  <div class="wrapper">
+    <h1>jjjjj</h1>
+  </div>
+</section>
+<!--banner section end-->
